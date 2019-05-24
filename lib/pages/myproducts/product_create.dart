eaf63bdf676fff0b759416e34866f64fff0e633f
cgustav <gstv.qnd@outlook.com>
2019-05-24 16:24:32 -0400

    done with models and routing

--- lib/pages/myproducts/product_create.dart
+++ lib/pages/myproducts/product_create.dart
@@ -5,7 +5,18 @@ class ProductCreateTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Under construction <3'),
+        child: RaisedButton(child: Text('Save'), 
+        onPressed: (){
+          //it slides up a sheet from the bottom of the page
+          //into which you can put additional information
+          showModalBottomSheet(
+          context: context, 
+          builder: (BuildContext context){
+            return Center(child: Text('WENA'),);
+          }
+          );
+        },
+        ),
       ),
     );
     //return Center(child: Text('Under Construction'),);
